830548dfb7b1a9ff58ac7c5b9645f8c19a3aea6a
Muhammad Rahman <rahman.muraman@gmail.com>
2022-03-15 14:10:05 +0700

    add 10_Introduction React

---
+++ 10_Introduction React/praktikum/mytask/src/App.js
@@ -0,0 +1,13 @@
+import Home from './Home';
+import Contact_Us from './Contact_Us';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Contact_Us/>
+    </div>
+  );
+}
+
+export default App;
