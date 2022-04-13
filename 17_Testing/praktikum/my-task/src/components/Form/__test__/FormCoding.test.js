7439b7206636693370dc1a74eb20e449fe12d2a9
Muhammad Rahman <rahman.muraman@gmail.com>
2022-04-13 22:07:11 +0700

    add test

---
+++ 17_Testing/praktikum/my-task/src/components/Form/__test__/FormCoding.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import Form from "../FormCoding";
+
+test("renders Form Component", () => {
+  render(<Form />);
+});
---
+++ 17_Testing/praktikum/my-task/src/components/InputValue/__test__/useInputValue.test.js
@@ -0,0 +1,8 @@
+// import { render, screen } from "@testing-library/react";
+// import Input from "../useInputValue";
+
+// test("renders Input Component", () => {
+//   render(<Input />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
